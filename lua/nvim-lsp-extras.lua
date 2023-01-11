e8e58ed92f3b094cfde7046c0210793d6d5726fb
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-01-11 20:43:26 +0100

    small refactor

--- lua/nvim-lsp-extras.lua
+++ lua/nvim-lsp-extras.lua
@@ -5,11 +5,10 @@ M.setup = function(opts)
 	config.set(opts)
 
 	for conf, _ in pairs(config.get()) do
-		local extra = config.get(conf)
-		if extra then
+		if config.get(conf) then
 			vim.api.nvim_create_autocmd("LspAttach", {
 				pattern = "*",
-				group = vim.api.nvim_create_augroup(string.format("%sLspExtra", extra), { clear = true }),
+				group = vim.api.nvim_create_augroup(string.format("%sLspExtra", conf), { clear = true }),
 				callback = function(args)
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
 					require(string.format("nvim-lsp-extras.%s", conf)).setup(client)
