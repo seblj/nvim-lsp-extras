b7258741b2a3213559dd0710b2560a3100abcfa4
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-01-13 16:53:31 +0100

    override signature help as a hack to highlight active param

--- lua/nvim-lsp-extras/signature.lua
+++ lua/nvim-lsp-extras/signature.lua
@@ -80,4 +80,38 @@ M.setup = function(client)
     })
 end
 
+-- Hack to highlight active signature because of `open_floating_preview`
+-- override I have
+vim.lsp.handlers.signature_help = function(_, result, ctx, config)
+    config = config or {}
+    config.focus_id = ctx.method
+    if vim.api.nvim_get_current_buf() ~= ctx.bufnr then
+        -- Ignore result since buffer changed. This happens for slow language servers.
+        return
+    end
+    -- When use `autocmd CompleteDone <silent><buffer> lua vim.lsp.buf.signature_help()` to call signatureHelp handler
+    -- If the completion item doesn't have signatures It will make noise. Change to use `print` that can use `<silent>` to ignore
+    if not (result and result.signatures and result.signatures[1]) then
+        if config.silent ~= true then
+            print("No signature help available")
+        end
+        return
+    end
+    local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+    local triggers = vim.tbl_get(client.server_capabilities, "signatureHelpProvider", "triggerCharacters")
+    local ft = vim.bo[ctx.bufnr].filetype
+    local lines, hl = util.convert_signature_help_to_markdown_lines(result, ft, triggers)
+    if not lines or vim.tbl_isempty(lines) then
+        if config.silent ~= true then
+            print("No signature help available")
+        end
+        return
+    end
+    local fbuf, fwin = util.open_floating_preview(lines, "markdown", config)
+    if hl then
+        vim.api.nvim_buf_add_highlight(fbuf, -1, "LspSignatureActiveParameter", 0, unpack(hl))
+    end
+    return fbuf, fwin
+end
+
 return M
