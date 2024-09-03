1bd6cbbe3c3cbc056b643ce83edf2de8b3e421e7
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-09-03 10:34:13 +0200

    simplify signature

--- lua/nvim-lsp-extras/signature.lua
+++ lua/nvim-lsp-extras/signature.lua
@@ -3,41 +3,20 @@ local util = require("vim.lsp.util")
 local handler
 local clients = {}
 
-local check_trigger_char = function(line_to_cursor, triggers)
-    if not triggers then
-        return false
-    end
-
-    for _, trigger_char in ipairs(triggers) do
-        local current_char = line_to_cursor:sub(#line_to_cursor, #line_to_cursor)
-        local prev_char = line_to_cursor:sub(#line_to_cursor - 1, #line_to_cursor - 1)
-        if current_char == trigger_char then
-            return true
-        end
-        if current_char == " " and prev_char == trigger_char then
-            return true
-        end
-    end
-    return false
-end
-
 local open_signature = function()
-    local triggered = false
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local line_to_cursor = vim.api.nvim_get_current_line():sub(pos[2] - 1, pos[2])
 
     for _, client in pairs(clients) do
-        local pos = vim.api.nvim_win_get_cursor(0)
-        local line = vim.api.nvim_get_current_line()
-        local line_to_cursor = line:sub(1, pos[2])
+        for _, trigger_char in ipairs(client.server_capabilities.signatureHelpProvider.triggerCharacters or {}) do
+            local current_char = line_to_cursor:sub(#line_to_cursor, #line_to_cursor)
+            local prev_char = line_to_cursor:sub(#line_to_cursor - 1, #line_to_cursor - 1)
 
-        if not triggered then
-            local triggers = client.server_capabilities.signatureHelpProvider.triggerCharacters
-            triggered = check_trigger_char(line_to_cursor, triggers)
+            if current_char == trigger_char or (current_char == " " and prev_char == trigger_char) then
+                local params = util.make_position_params()
+                return vim.lsp.buf_request(0, "textDocument/signatureHelp", params, handler)
+            end
         end
-    end
-
-    if triggered then
-        local params = util.make_position_params()
-        vim.lsp.buf_request(0, "textDocument/signatureHelp", params, handler)
     end
 end
 
