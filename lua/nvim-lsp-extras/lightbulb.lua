fd919099b4af3957ddba4034d0744d10746f14b6
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-09-03 14:40:46 +0200

    fix: bugs with code action lightbulb

--- lua/nvim-lsp-extras/lightbulb.lua
+++ lua/nvim-lsp-extras/lightbulb.lua
@@ -1,55 +1,41 @@
-local lsp_util = require("vim.lsp.util")
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
 local sign_name = "LightbulbSign"
 local sign_group = "LightbulbGroup"
-local old_line = {}
+local old_line = nil
 local config = require("nvim-lsp-extras.config")
 local M = {}
 
-local changed_line = function()
-    local line = vim.fn.line(".")
-    if line == old_line[1] then
+local changed_line = function(lnum)
+    if lnum == old_line then
         return false
     end
-    old_line = { line }
+    old_line = lnum
     return true
 end
 
-local handler = function(results, ctx)
-    local res = results and results[1] or {}
-    local num_results = res.result and #res.result or 0
-    if num_results == 0 then
-        vim.fn.sign_unplace(sign_group)
-        return
-    end
-
-    -- Only show actions if there are diagnostics
-    if config.get("lightbulb").diagnostic_only and #vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 }) == 0 then
-        vim.fn.sign_unplace(sign_group)
-        return
-    end
-
-    if changed_line() then
-        vim.fn.sign_unplace(sign_group)
-    end
-    vim.fn.sign_place(0, sign_group, sign_name, ctx.bufnr, { lnum = vim.fn.line("."), priority = 1000 })
-end
-
-local check_code_action = function()
-    if vim.fn.mode() ~= "n" then
-        return
-    end
-    local bufnr = vim.api.nvim_get_current_buf()
-    local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
-    local context = {
-        diagnostics = vim.diagnostic.get(0, { lnum = lnum - 1 }),
+---@param client vim.lsp.Client
+local check_code_action = function(client, bufnr)
+    local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+    local params = vim.lsp.util.make_range_params()
+    params.context = {
+        diagnostics = vim.tbl_map(function(d)
+            return d.user_data.lsp
+        end, vim.diagnostic.get(0, { lnum = lnum })),
     }
-    local params = lsp_util.make_range_params()
-    params.context = context
-    local method = "textDocument/codeAction"
-    vim.lsp.buf_request_all(bufnr, method, params, function(results)
-        handler(results, { bufnr = bufnr, method = method, params = params })
+
+    client.request("textDocument/codeAction", params, function(_, results)
+        if #(results and results[1] or {}) > 0 then
+            return vim.fn.sign_unplace(sign_group)
+        end
+
+        -- Only show actions if there are diagnostics
+        if config.get("lightbulb").diagnostic_only and #params.context.diagnostics == 0 then
+            return vim.fn.sign_unplace(sign_group)
+        end
+
+        if changed_line(lnum) then
+            vim.fn.sign_unplace(sign_group)
+        end
+        vim.fn.sign_place(0, sign_group, sign_name, bufnr, { lnum = vim.fn.line("."), priority = 1000 })
     end)
 end
 
@@ -59,17 +45,20 @@ M.setup = function(client)
     end
     vim.fn.sign_define(sign_name, { text = config.get("lightbulb").icon, texthl = "DiagnosticInfo" })
 
-    local group = augroup("SetupLightbulb", { clear = false })
+    local group = vim.api.nvim_create_augroup("SetupLightbulb", { clear = false })
     vim.api.nvim_clear_autocmds({ group = group, pattern = "<buffer>" })
-    autocmd({ "CursorHold", "CursorMoved" }, {
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorMoved" }, {
         group = group,
         pattern = "<buffer>",
-        callback = function()
+        callback = function(args)
+            if vim.fn.mode() ~= "n" then
+                return
+            end
             -- Always check that at least one client is supporting this, as a server coul be stopped
             local active_clients = vim.lsp.get_clients()
             for _, c in ipairs(active_clients) do
                 if c.supports_method("textDocument/codeAction") then
-                    check_code_action()
+                    check_code_action(c, args.buf)
                 end
             end
         end,
